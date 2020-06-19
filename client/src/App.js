8697b009c9264899e94cff5c1a609d23ad9d5b08
sriganesh <sriganeshlokesh@gmail.com>
2020-06-19 17:00:21 -0500

    Add Post Component

--- client/src/App.js
+++ client/src/App.js
@@ -21,6 +21,7 @@ import AddEducation from "./components/add-credientials/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import NotFound from "./components/not-found/NotFound";
+import Posts from "./components/posts/Posts";
 
 // Check for token
 if (localStorage.jwtToken) {
@@ -86,6 +87,9 @@ function App() {
                 path="/add-education"
                 component={AddEducation}
               />
+            </Switch>
+            <Switch>
+              <PrivateRoute exact path="/feed" component={Posts} />
             </Switch>
             <Route exact path="/not-found" component={NotFound} />
           </div>
