e9f08c1b50b12a606c2c3977ae826e69b9be287e
sriganesh <sriganeshlokesh@gmail.com>
2020-06-20 13:23:43 -0500

    Post Component Complete

--- client/src/actions/postActions.js
+++ client/src/actions/postActions.js
@@ -1,5 +1,13 @@
 import axios from "axios";
-import { ADD_POST, GET_ERRORS, GET_POSTS, POST_LOADING } from "./types";
+import {
+  ADD_POST,
+  GET_ERRORS,
+  GET_POSTS,
+  GET_POST,
+  POST_LOADING,
+  DELETE_POST,
+  CLEAR_ERRORS,
+} from "./types";
 
 // Add Post
 export const addPost = (postData) => (dispatch) => {
@@ -19,8 +27,8 @@ export const addPost = (postData) => (dispatch) => {
     );
 };
 
-// Get Post
-export const getPost = () => (dispatch) => {
+// Get Posts
+export const getPosts = () => (dispatch) => {
   dispatch(setPostLoading());
   axios
     .get("/api/post")
@@ -38,9 +46,116 @@ export const getPost = () => (dispatch) => {
     );
 };
 
+// Get Post
+export const getPost = (id) => (dispatch) => {
+  dispatch(setPostLoading());
+  axios
+    .get(`/api/post/${id}`)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_POST,
+        payload: null,
+      })
+    );
+};
+
+// Delete Post
+export const deletePost = (id) => (dispatch) => {
+  axios
+    .delete(`/api/post/${id}`)
+    .then((res) =>
+      dispatch({
+        type: DELETE_POST,
+        payload: id,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Add Like
+export const addLike = (id) => (dispatch) => {
+  axios
+    .post(`/api/post/like/${id}`)
+    .then((res) => dispatch(getPosts()))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Remove Like
+export const removeLike = (id) => (dispatch) => {
+  axios
+    .post(`/api/post/unlike/${id}`)
+    .then((res) => dispatch(getPosts()))
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Add Comment
+export const addComment = (postId, commentData) => (dispatch) => {
+  dispatch(clearErrors());
+  axios
+    .post(`/api/post/comment/${postId}`, commentData)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
+// Delete Comment
+export const deleteComment = (postId, commentId) => (dispatch) => {
+  axios
+    .delete(`/api/post/comment/${postId}/${commentId}`)
+    .then((res) =>
+      dispatch({
+        type: GET_POST,
+        payload: res.data,
+      })
+    )
+    .catch((err) =>
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data,
+      })
+    );
+};
+
 // Set Loading State
 export const setPostLoading = () => {
   return {
     type: POST_LOADING,
+  };
+};
+
+// Clear errors
+export const clearErrors = () => {
+  return {
+    type: CLEAR_ERRORS,
   };
 };
